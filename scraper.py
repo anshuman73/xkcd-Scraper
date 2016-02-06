c8ce2636b601e23daeed8dbc965e479b4e388d1e
Prodge <prodge@prodge.net>
2016-02-06 16:09:13 +0800

    Refactored download into seperate function

--- scraper.py
+++ scraper.py
@@ -8,11 +8,21 @@ import urllib
 import os
 from optparse import OptionParser
 
+def download_image(number, directory, prefix=""):
+    url = 'http://xkcd.com/{}/info.0.json'.format(number)
+    img_metadata = json.loads(urllib.urlopen(url).read())
+    image_file = '{}{}-{}'.format(prefix, number, img_metadata['safe_title'])
+    file_path = '{}/{}'.format(directory, image_file)
+    if not os.path.exists(file_path):
+        print 'Downloading image: ' + image_file
+        urllib.urlretrieve(img_metadata['img'], file_path)
+    else:
+        print 'Image' + image_file + '" already exists. Skipping...'
+
 def download_all(directory, prefix=""):
     print 'Querying Total Number of Images as of now...'
     total = json.loads(urllib.urlopen('http://xkcd.com/info.0.json').read())['num']
     print 'Querying complete\n\nTotal of ' + str(total) + ' images found\n\n'
-    base_url = 'http://xkcd.com/%s/info.0.json'
     print 'Checking if /images directory exists...\n'
     if not os.path.exists(directory):
         print 'Making directory {}'.format(directory)
@@ -21,16 +31,8 @@ def download_all(directory, prefix=""):
         if num == 404:
             print '\n\nImage 404 is a pathetic little joke -__- Ignoring...\n\n'
             continue
+        download_image(num, directory, prefix=prefix)
 
-        url = base_url % (num)
-        img_metadata = json.loads(urllib.urlopen(url).read())
-        image_file = '{}{}-{}'.format(prefix, num, img_metadata['safe_title'])
-        file_path = '{}/{}'.format(directory, image_file)
-        if not os.path.exists(file_path):
-            print 'Downloading image: ' + image_file
-            urllib.urlretrieve(img_metadata['img'], file_path)
-        else:
-            print 'Image' + image_file + '" already exists. Skipping...'
 
 def main():
     parser = OptionParser(usage="usage: python scraper.py [options]")
