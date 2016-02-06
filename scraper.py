e0eaff76fa860a7558b153a99297fb04ee1c28c4
Prodge <prodge@prodge.net>
2016-02-06 15:59:17 +0800

    Added optarg for setting the download directory

--- scraper.py
+++ scraper.py
@@ -8,28 +8,29 @@ import urllib
 import os
 from optparse import OptionParser
 
-def download_all():
-	print 'Querying Total Number of Images as of now...'
-	total = json.loads(urllib.urlopen('http://xkcd.com/info.0.json').read())['num']
-	print 'Querying complete\n\nTotal of ' + str(total) + ' images found\n\n'
-	base_url = 'http://xkcd.com/%s/info.0.json'
-	wd = os.getcwd()
-	print 'Checking if /images directory exists...\n'
-	if not os.path.exists(wd + '/images'):
-		print 'Making directory /images\n'
-		os.makedirs(wd + '/images')
-	for num in xrange(1, total + 1):
-		if num == 404:
-			print '\n\nImage 404 is a pathetic little joke -__- Ignoring...\n\n'
-			continue
+def download_all(directory):
+    print 'Querying Total Number of Images as of now...'
+    total = json.loads(urllib.urlopen('http://xkcd.com/info.0.json').read())['num']
+    print 'Querying complete\n\nTotal of ' + str(total) + ' images found\n\n'
+    base_url = 'http://xkcd.com/%s/info.0.json'
+    print 'Checking if /images directory exists...\n'
+    if not os.path.exists(directory):
+        print 'Making directory {}'.format(directory)
+        os.makedirs(directory)
+    for num in xrange(1, total + 1):
+        if num == 404:
+            print '\n\nImage 404 is a pathetic little joke -__- Ignoring...\n\n'
+            continue
 
-		url = base_url % (num)
-		img_metadata = json.loads(urllib.urlopen(url).read())
-		if not os.path.exists(wd + '/images/' + str(num) + ' - ' + img_metadata['safe_title']):
-			print 'Downloading image ' + str(num) + ' - "' + img_metadata['safe_title'] + '"'
-			urllib.urlretrieve(img_metadata['img'], wd + '/images/' + str(num) + ' - ' + img_metadata['safe_title'])
-		else:
-			print 'Image' + str(num) + ' - "' + img_metadata['safe_title'] + '" already exists. Skipping...'
+        url = base_url % (num)
+        img_metadata = json.loads(urllib.urlopen(url).read())
+        image_file = '{}-{}'.format(num, img_metadata['safe_title'])
+        file_path = '{}/{}'.format(directory, image_file)
+        if not os.path.exists(file_path):
+            print 'Downloading image: ' + image_file
+            urllib.urlretrieve(img_metadata['img'], file_path)
+        else:
+            print 'Image' + image_file + '" already exists. Skipping...'
 
 def main():
     parser = OptionParser(usage="usage: python scraper.py [options]")
@@ -38,9 +39,19 @@ def main():
         action = "store_true",
         default = False
     )
+    parser.add_option(
+        "-d", "--directory",
+        action = "store",
+        default = os.getcwd() + '/images'
+    )
     (options, args) = parser.parse_args()
 
+    #Cleaning directory input - Removing / at the end if it was entered
+    if options.directory[-1] == '/':
+        options.directory = options.directory[0:-1]
+
     if options.autostart or raw_input('\nPress Enter to start:') == '':
-        download_all()
+        download_all(options.directory)
+
 
 main()
